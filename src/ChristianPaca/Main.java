bd053a134a3b28cc28c50da63f7ba012685488c3
PacaChristianPhilipS27 <cybercion.xtian@gmail.com>
2023-11-17 14:08:05 +0800

    first commit

---
+++ src/ChristianPaca/Main.java
@@ -0,0 +1,24 @@
+
+package ChristianPaca;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Favorite sudan= new Favorite ("");
+      
+       sudan.adubo();
+       sudan.halanghalang();
+       
+
+    }
+    
+    }
+    
+    
+
+    
+
