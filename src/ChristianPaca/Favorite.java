0780e2049a08cf447a5ef2e4cd47da4778f113d8
PacaChristianPhilipS27 <cybercion.xtian@gmail.com>
2023-11-17 14:23:16 +0800

    added branch

---
+++ src/ChristianPaca/Favorite.java
@@ -0,0 +1,17 @@
+
+package ChristianPaca;
+
+/**
+ *
+ * @author Admin
+ */
+public class Favorite extends food {
+    
+    Favorite(String name){
+       super (name);
+       
+    }
+    void adubo(){
+        System.out.println(name + " is yummy");
+    }
+}
